091ad265568b6800ea96609364ebf8f1ee034efb
CamilaRanniele <rannielecamsil@gmail.com>
2022-05-31 17:22:38 -0300

    create teams model

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+  tableName: 'teams',
+});
+
+export default Teams;
