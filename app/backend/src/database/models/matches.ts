067e68a66773758227a45816496a3c24cb21165c
CamilaRanniele <rannielecamsil@gmail.com>
2022-05-31 17:21:09 -0300

    create matches model

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,56 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+import Teams from './teams';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+  tableName: 'matches',
+});
+
+Teams.belongsTo(Matches, { foreignKey: 'homeTeam', as: 'home_team' });
+Teams.belongsTo(Matches, { foreignKey: 'awayTeam', as: 'away_team' });
+
+Matches.hasMany(Teams, { foreignKey: 'homeTeam', as: 'home_team' });
+Matches.hasMany(Teams, { foreignKey: 'awayTeam', as: 'away_team' });
+
+export default Matches;
