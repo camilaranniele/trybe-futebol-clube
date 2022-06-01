8e08691921c27842240492023343eb1f25b452ad
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-01 18:59:45 -0300

    feat: create login services

---
+++ app/backend/src/services/Login.service.ts
@@ -0,0 +1,33 @@
+import * as Bcrypt from 'bcrypt';
+import IUser from '../interfaces/IUser';
+import { createToken } from '../util/Token';
+import User from '../database/models/users';
+
+class LoginService {
+  public findUser: IUser | null;
+
+  public async login(useremail: string, password: string) {
+    this.findUser = await User
+      .findOne({ where: { email: useremail } });
+
+    if (!this.findUser) throw new Error('User not found');
+
+    const compareWithBcrypt = Bcrypt.compare(password, this.findUser.password as string);
+
+    if (!compareWithBcrypt) throw new Error('Incorrect password');
+    const { id, username, role, email } = this.findUser;
+    const token = createToken(this.findUser);
+
+    return {
+      user: {
+        id,
+        username,
+        role,
+        email,
+      },
+      token,
+    };
+  }
+}
+
+export default LoginService;
