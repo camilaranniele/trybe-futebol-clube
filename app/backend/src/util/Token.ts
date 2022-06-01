1deebd4a15fc2139950128b5941c19d94cb5652f
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-01 18:58:52 -0300

    feat: create token generator

---
+++ app/backend/src/util/Token.ts
@@ -0,0 +1,18 @@
+import jwt = require('jsonwebtoken');
+import fs = require('fs');
+
+const secret = fs.readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
+
+export const createToken = (user: object | null) => {
+  const token = jwt.sign({ data: user }, secret, {
+    expiresIn: '20d',
+    algorithm: 'HS256',
+  });
+
+  return token;
+};
+
+export const decoderToken = (token: string) => {
+  const decoder = jwt.verify(token, secret);
+  return decoder;
+};
