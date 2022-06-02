741e2f84df5ea5f03b5293d77e6ab3d7344c9385
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-02 12:49:47 -0300

    create middleware to validate login

---
+++ app/backend/src/middlewares/user.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { Schema } from 'joi';
+
+class userValidation {
+  public validate = (req: Request, res: Response, next: NextFunction, schema: Schema) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      if (error.details[0].type === 'string.email') {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: error.details[0].message });
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: error.details[0].message });
+    }
+    next();
+  };
+}
+
+export default userValidation;
