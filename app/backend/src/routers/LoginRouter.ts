8e752e1250e1faebd4abaf14f61757209163acba
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-02 12:50:17 -0300

    add middleware in login route

--- app/backend/src/routers/LoginRouter.ts
+++ app/backend/src/routers/LoginRouter.ts
@@ -1,11 +1,19 @@
 import { Application } from 'express';
+import UserValidation from '../middlewares/user.middleware';
 import LoginControler from '../controllers/Login.controller';
+import LoginSchema from '../middlewares/schemas/login.schema';
 
 class LoginRouter {
   public loginController = new LoginControler();
+  public userValidation = new UserValidation();
 
   public route = (app: Application) => {
-    app.post('/login', (req, res) => this.loginController.login(req, res));
+    app.post(
+      '/login',
+      (req, res, next) => this.userValidation.validate(req, res, next, LoginSchema),
+
+      (req, res) => this.loginController.login(req, res),
+    );
   };
 }
 
