d651a05d033969963db7053fcfe1ddeca10a9eef
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-03 12:00:45 -0300

    feat: get team by id

--- app/backend/src/services/Teams.service.ts
+++ app/backend/src/services/Teams.service.ts
@@ -3,11 +3,17 @@ import ITeam from '../interfaces/ITeams';
 
 class TeamsService {
   public teams: Array<ITeam[] | ITeam>;
+  public team: ITeam | null;
 
   public async getAllTeams() {
     this.teams = await Teams.findAll();
 
     return this.teams;
+  }
+
+  public async getTeamById(id: number) {
+    this.team = await Teams.findOne({ where: { id } });
+    return this.team;
   }
 }
 
