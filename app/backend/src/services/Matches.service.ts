df06eb2bcade723b20300bd714dd76b6118ab8fb
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-03 18:00:23 -0300

    feat: adiciona filtro de busca por query

--- app/backend/src/services/Matches.service.ts
+++ app/backend/src/services/Matches.service.ts
@@ -4,24 +4,47 @@ import IMatches from '../interfaces/IMatches';
 
 class MatcheService {
   public matches: Array<IMatches[] | IMatches>;
+  private _query = {
+    where: {},
+    include: [
+      {
+        model: Teams,
+        as: 'teamHome',
+        attributes: { exclude: ['id'] },
+      },
+      {
+        model: Teams,
+        as: 'teamAway',
+        attributes: { exclude: ['id'] },
+      },
+    ],
+  };
 
-  public async getAllMatches() {
-    this.matches = await Matches.findAll({
-      include: [
-        {
-          model: Teams,
-          as: 'teamHome',
-          attributes: { exclude: ['id'] },
-        },
-        {
-          model: Teams,
-          as: 'teamAway',
-          attributes: { exclude: ['id'] },
-        },
-      ],
-    });
-    return this.matches;
-  }
+  public getAllMatches = async (inProgress: boolean | null | undefined) => {
+    if (inProgress !== null && inProgress !== undefined) {
+      this._query.where = { inProgress };
+    }
+    const matches = await Matches.findAll(this._query);
+    return matches;
+  };
+
+  // public async getAllMatches() {
+  //   this.matches = await Matches.findAll({
+  //     include: [
+  //       {
+  //         model: Teams,
+  //         as: 'teamHome',
+  //         attributes: { exclude: ['id'] },
+  //       },
+  //       {
+  //         model: Teams,
+  //         as: 'teamAway',
+  //         attributes: { exclude: ['id'] },
+  //       },
+  //     ],
+  //   });
+  //   return this.matches;
+  // }
 }
 
 export default MatcheService;
