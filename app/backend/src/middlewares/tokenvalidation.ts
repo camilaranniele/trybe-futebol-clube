62d0ef425168042609da6df0feb303b977fc76d4
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-05 13:17:46 -0300

    cria arquivo para validar se o token é valido

---
+++ app/backend/src/middlewares/tokenvalidation.ts
@@ -0,0 +1,21 @@
+import { verify } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import fs = require('fs');
+
+const secret = fs.readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization: token } = req.headers;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  try {
+    verify(token, secret);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+export default verifyToken;
