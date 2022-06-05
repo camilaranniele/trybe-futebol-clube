566d3bcf0126112f40ea037d7431ef5733e98880
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-05 16:06:29 -0300

    modifica erro de retorno de middlewares

--- app/backend/src/middlewares/schemas/login.schema.ts
+++ app/backend/src/middlewares/schemas/login.schema.ts
@@ -1,13 +1,16 @@
 import Joi = require('joi');
 
 const loginSchema = Joi.object().keys({
-  email: Joi.string().email().empty().required()
+  email: Joi.string().email().required()
     .messages({
       'string.empty': 'All fields must be filled',
       'string.email': 'Incorrect email or password',
     }),
   password: Joi.string().min(6).required()
-    .messages({ 'string.empty': 'All fields must be filled' }),
+    .messages({
+      'string.empty': 'All fields must be filled',
+      'string.min': 'Incorrect email or password',
+    }),
 });
 
 export default loginSchema;
