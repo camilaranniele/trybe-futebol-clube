4981af28b0f99996b86f13ec8c728d5bdb93f4af
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-07 16:04:03 -0300

    feat: renomea funcao e cria funcao para retornar leaderboard away

--- app/backend/src/controllers/Leaderboards.controller.ts
+++ app/backend/src/controllers/Leaderboards.controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
+import LeaderboardAwayService from '../services/LeaderBoardAway.service';
 import LeaderboardHomeService from '../services/LeaderBoardHome.service';
 
 class LeaderBoardController {
   private _leaderboardHomeService = new LeaderboardHomeService();
-  async getAll(req: Request, res: Response) {
+  private _leaderboardAwayService = new LeaderboardAwayService();
+
+  async getHomeBoard(req: Request, res: Response) {
     const board = await this._leaderboardHomeService.getBoard();
+    res.status(200).json(board);
+  }
+
+  async getAwayBoard(req: Request, res: Response) {
+    const board = await this._leaderboardAwayService.getBoard();
     res.status(200).json(board);
   }
 }
