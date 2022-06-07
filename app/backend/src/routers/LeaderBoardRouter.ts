0262f20c63ae1905218d2c5643d4881c798a7a78
CamilaRanniele <rannielecamsil@gmail.com>
2022-06-07 16:04:26 -0300

    feat: leaderboard away  rotas

--- app/backend/src/routers/LeaderBoardRouter.ts
+++ app/backend/src/routers/LeaderBoardRouter.ts
@@ -3,10 +3,15 @@ import LeaderBoardHomeController from '../controllers/Leaderboards.controller';
 
 class LeaderboardRouter {
   public leaderBoardHomeController = new LeaderBoardHomeController();
+
   public route = (app: Application) => {
     app.get(
       '/leaderboard/home',
-      (req, res) => this.leaderBoardHomeController.getAll(req, res),
+      (req, res) => this.leaderBoardHomeController.getHomeBoard(req, res),
+    );
+    app.get(
+      '/leaderboard/away',
+      (req, res) => this.leaderBoardHomeController.getAwayBoard(req, res),
     );
   };
 }
